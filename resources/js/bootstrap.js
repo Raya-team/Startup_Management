7f64b644cb2444f15366c3b7a33c3db645f9e795
Ehsan <ehsanroozbakhsh@gmail.com>
2021-08-11 15:40:08 +0430

    Ehsan

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,13 @@
 window._ = require('lodash');
-
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
  */
-
+window.Vue = Vue;
+Vue.use(VueRouter);
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
