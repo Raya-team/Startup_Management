097a10b84676977cd812ef795da880a68d893efa
Ehsan <ehsanroozbakhsh@gmail.com>
2021-08-12 11:41:44 +0430

    Ehsan

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -1,9 +1,22 @@
+
 <!DOCTYPE html>
+<!--
+Template Name: Metronic - Bootstrap 4 HTML, React, Angular 11 & VueJS Admin Dashboard Theme
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: https://1.envato.market/EA4JP
+Renew Support: https://1.envato.market/EA4JP
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
 <html direction="rtl" dir="rtl" style="direction: rtl" >
 <!--begin::Head-->
 <head><base href="../../../../">
     <meta charset="utf-8" />
-    <title>@yield('title')</title>
+    <title>Sign In | Keenthemes</title>
     <meta name="description" content="Singin page example" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="canonical" href="https://keenthemes.com/metronic" />
@@ -11,7 +24,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Page Custom Styles(used by this page)-->
-    <link href="assets/css/pages/login/login-4.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/pages/login/login-3.css" rel="stylesheet" type="text/css" />
     <!--end::Page Custom Styles-->
     <!--begin::Global Theme Styles(used by all pages)-->
     <link href="assets/plugins/global/plugins.bundle.rtl.css" rel="stylesheet" type="text/css" />
@@ -25,13 +38,14 @@
     <link href="assets/css/themes/layout/aside/dark.rtl.css" rel="stylesheet" type="text/css" />
     <!--end::Layout Themes-->
     <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
-    @yield('css')
 </head>
 <!--end::Head-->
 <!--begin::Body-->
 <body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
 <!--begin::Main-->
-@yield('content')
+<div class="d-flex flex-column flex-root">
+    @yield('content')
+</div>
 <!--end::Main-->
 <script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
 <!--begin::Global Config(global config for global JS scripts)-->
@@ -43,8 +57,7 @@
 <script src="assets/js/scripts.bundle.js"></script>
 <!--end::Global Theme Bundle-->
 <!--begin::Page Scripts(used by this page)-->
-<script src="assets/js/pages/custom/login/login-4.js"></script>
-@yield('javascript')
+<script src="assets/js/pages/custom/login/login-3.js"></script>
 <!--end::Page Scripts-->
 </body>
 <!--end::Body-->
