a3ca225212d8542d802a37882bfb45a69daf9909
Ehsan <ehsanroozbakhsh@gmail.com>
2021-08-12 11:47:04 +0430

    Ehsan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,70 +3,70 @@
 @section('content')
     <!--begin::Main-->
     <div class="d-flex flex-column flex-root">
-    <!--begin::Login-->
-    <div class="login login-3 wizard d-flex flex-column flex-lg-row flex-column-fluid">
-        <!--begin::Aside-->
-        <div class="login-aside d-flex flex-column flex-row-auto">
-            <!--begin::Aside Top-->
-            <div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
-                <!--begin::Aside header-->
-                <a href="#" class="login-logo text-center pt-lg-25 pb-10">
-                    <img src="assets/media/logos/logo-1.png" class="max-h-70px" alt="" />
-                </a>
-                <!--end::Aside header-->
-                <!--begin::Aside Title-->
-                <h3 class="font-weight-bolder text-center font-size-h4 text-dark-50 line-height-xl">User Experience &amp; Interface Design
-                    <br />Strategy SaaS Solutions</h3>
-                <!--end::Aside Title-->
-            </div>
-            <!--end::Aside Top-->
-            <!--begin::Aside Bottom-->
+        <!--begin::Login-->
+        <div class="login login-3 wizard d-flex flex-column flex-lg-row flex-column-fluid">
+            <!--begin::Aside-->
+            <div class="login-aside d-flex flex-column flex-row-auto">
+                <!--begin::Aside Top-->
+                <div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
+                    <!--begin::Aside header-->
+                    <a href="#" class="login-logo text-center pt-lg-25 pb-10">
+                        <img src="assets/media/logos/logo-1.png" class="max-h-70px" alt="" />
+                    </a>
+                    <!--end::Aside header-->
+                    <!--begin::Aside Title-->
+                    <h3 class="font-weight-bolder text-center font-size-h4 text-dark-50 line-height-xl">User Experience &amp; Interface Design
+                        <br />Strategy SaaS Solutions</h3>
+                    <!--end::Aside Title-->
+                </div>
+                <!--end::Aside Top-->
+                <!--begin::Aside Bottom-->
             {{--<div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-x-center" style="background-position-y: calc(100% + 5rem); background-image: url(assets/media/svg/illustrations/login-visual-5.svg)"></div>--}}
             <!--end::Aside Bottom-->
-        </div>
-        <!--begin::Aside-->
-        <!--begin::Content-->
-        <div class="login-content flex-row-fluid d-flex flex-column p-10">
-            <!--begin::Top-->
-            <div class="text-right d-flex justify-content-center">
-                <div class="top-signin text-right d-flex justify-content-end pt-5 pb-lg-0 pb-10">
-                    <span class="font-weight-bold text-muted font-size-h4">Having issues?</span>
-                    <a href="javascript:;" class="font-weight-bold text-primary font-size-h4 ml-2" id="kt_login_signup">Get Help</a>
-                </div>
             </div>
-            <!--end::Top-->
-            <!--begin::Wrapper-->
-            <div class="d-flex flex-row-fluid flex-center">
-                <!--begin::Signin-->
-                <div class="login-form">
-                    <!--begin::Form-->
-                    <form class="form" id="kt_login_singin_form" action="">
-                        <!--begin::Title-->
-                        <div class="pb-5 pb-lg-15">
-                            <h3 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">ورود</h3>
-                            <div class="text-muted font-weight-bold font-size-h4">حساب ندارید؟
-                                <a href="{{ route('register') }}" class="text-primary font-weight-bolder">ایجاد حساب</a></div>
-                        </div>
-                        <!--begin::Title-->
-                        <!--begin::Form group-->
-                        <div class="form-group">
-                            <label class="font-size-h6 font-weight-bolder text-dark">ایمیل</label>
-                            <input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="text" name="username" autocomplete="off" />
-                        </div>
-                        <!--end::Form group-->
-                        <!--begin::Form group-->
-                        <div class="form-group">
-                            <div class="d-flex justify-content-between mt-n5">
-                                <label class="font-size-h6 font-weight-bolder text-dark pt-5">رمز عبور</label>
-                                <a href="{{ route('password.request') }}" class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5">رمز عبور خود را فراموش کرده اید؟</a>
+            <!--begin::Aside-->
+            <!--begin::Content-->
+            <div class="login-content flex-row-fluid d-flex flex-column p-10">
+                <!--begin::Top-->
+                <div class="text-right d-flex justify-content-center">
+                    <div class="top-signin text-right d-flex justify-content-end pt-5 pb-lg-0 pb-10">
+                        <span class="font-weight-bold text-muted font-size-h4">Having issues?</span>
+                        <a href="javascript:;" class="font-weight-bold text-primary font-size-h4 ml-2" id="kt_login_signup">Get Help</a>
+                    </div>
+                </div>
+                <!--end::Top-->
+                <!--begin::Wrapper-->
+                <div class="d-flex flex-row-fluid flex-center">
+                    <!--begin::Signin-->
+                    <div class="login-form">
+                        <!--begin::Form-->
+                        <form class="form" id="kt_login_singin_form" action="">
+                            <!--begin::Title-->
+                            <div class="pb-5 pb-lg-15">
+                                <h3 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">ورود</h3>
+                                <div class="text-muted font-weight-bold font-size-h4">حساب ندارید؟
+                                    <a href="{{ route('register') }}" class="text-primary font-weight-bolder">ایجاد حساب</a></div>
                             </div>
-                            <input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="password" name="password" autocomplete="off" />
-                        </div>
-                        <!--end::Form group-->
-                        <!--begin::Action-->
-                        <div class="pb-lg-0 pb-5">
-                            <button type="submit" id="kt_login_singin_form_submit_button" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">Sign In</button>
-                            <button type="button" class="btn btn-light-primary font-weight-bolder px-8 py-4 my-3 font-size-lg">
+                            <!--begin::Title-->
+                            <!--begin::Form group-->
+                            <div class="form-group">
+                                <label class="font-size-h6 font-weight-bolder text-dark">ایمیل</label>
+                                <input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="text" name="username" autocomplete="off" />
+                            </div>
+                            <!--end::Form group-->
+                            <!--begin::Form group-->
+                            <div class="form-group">
+                                <div class="d-flex justify-content-between mt-n5">
+                                    <label class="font-size-h6 font-weight-bolder text-dark pt-5">رمز عبور</label>
+                                    <a href="{{ route('password.request') }}" class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5">رمز عبور خود را فراموش کرده اید؟</a>
+                                </div>
+                                <input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="password" name="password" autocomplete="off" />
+                            </div>
+                            <!--end::Form group-->
+                            <!--begin::Action-->
+                            <div class="pb-lg-0 pb-5">
+                                <button type="submit" id="kt_login_singin_form_submit_button" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">Sign In</button>
+                                <button type="button" class="btn btn-light-primary font-weight-bolder px-8 py-4 my-3 font-size-lg">
 									<span class="svg-icon svg-icon-md">
 										<!--begin::Svg Icon | path:assets/media/svg/social-icons/google.svg-->
 										<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
@@ -77,18 +77,18 @@
 										</svg>
                                         <!--end::Svg Icon-->
 									</span>Sign in with Google</button>
-                        </div>
-                        <!--end::Action-->
-                    </form>
-                    <!--end::Form-->
+                            </div>
+                            <!--end::Action-->
+                        </form>
+                        <!--end::Form-->
+                    </div>
+                    <!--end::Signin-->
                 </div>
-                <!--end::Signin-->
+                <!--end::Wrapper-->
             </div>
-            <!--end::Wrapper-->
+            <!--end::Content-->
         </div>
-        <!--end::Content-->
-    </div>
-    <!--end::Login-->
+        <!--end::Login-->
     </div>
     <!--end::Main-->
 @endsection
