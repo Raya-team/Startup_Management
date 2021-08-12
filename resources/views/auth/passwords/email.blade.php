2c78e281efbccafb4172ec0a34d5e59dc0e9d501
Ehsan <ehsanroozbakhsh@gmail.com>
2021-08-12 12:37:40 +0430

    Ehsan

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,28 +4,42 @@
     <!--begin::Main-->
     <div class="d-flex flex-column flex-root">
         <!--begin::Login-->
-        <div class="login login-4 wizard d-flex flex-column flex-lg-row flex-column-fluid">
+        <div class="login login-3 wizard d-flex flex-column flex-lg-row flex-column-fluid">
+            <!--begin::Aside-->
+            <div class="login-aside d-flex flex-column flex-row-auto">
+                <!--begin::Aside Top-->
+                <div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
+                    <!--begin::Aside Header-->
+                    <a href="#" class="login-logo text-center pt-lg-25 pb-10">
+                        <img src="assets/media/logos/logo-1.png" class="max-h-70px" alt="" />
+                    </a>
+                    <!--end::Aside Header-->
+                    <!--begin::Aside Title-->
+                    <h3 class="font-weight-bolder text-center font-size-h4 text-dark-50 line-height-xl">User Experience &amp; Interface Design
+                        <br />Strategy SaaS Solutions</h3>
+                    <!--end::Aside Title-->
+                </div>
+                <!--end::Aside Top-->
+
+            </div>
+            <!--begin::Aside-->
             <!--begin::Content-->
-            <div class="login-container order-2 order-lg-1 d-flex flex-center flex-row-fluid px-7 pt-lg-0 pb-lg-0 pt-4 pb-6 bg-white">
+            <div class="login-content flex-column-fluid d-flex flex-column p-10">
                 <!--begin::Wrapper-->
-                <div class="login-content d-flex flex-column pt-lg-0 pt-12">
-                    <!--begin::Logo-->
-                    {{--<a href="#" class="login-logo pb-xl-20 pb-15">--}}
-                        {{--<img src="assets/media/logos/logo-4.png" class="max-h-70px" alt="" />--}}
-                    {{--</a>--}}
-                    <!--end::Logo-->
-                    <!--begin::Signin-->
+                <div class="d-flex flex-row-fluid flex-center">
+                    <!--begin::Forgot-->
                     <div class="login-form">
+                        <!--begin::Form-->
                         <form class="form" id="kt_login_forgot_form" action="">
                             <!--begin::Title-->
                             <div class="pb-5 pb-lg-15">
                                 <h3 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">رمز عبور خود را فراموش کرده‌اید؟</h3>
-                                <p class="text-muted font-weight-bold font-size-h4">جهت نغییر رمز عبور ایمیل حساب کاربری خود را وارد کنید</p>
+                                <p class="text-muted font-weight-bold font-size-h4">لطفا ایمیل حساب کاربری خود را جهت درخواست تغییر رمز عبور وارد فرمایید</p>
                             </div>
                             <!--end::Title-->
                             <!--begin::Form group-->
                             <div class="form-group">
-                                <input class="form-control form-control-solid h-auto py-7 px-6 border-0 rounded-lg font-size-h6" type="email" placeholder="ایمیل" name="email" autocomplete="off" />
+                                <input class="form-control h-auto py-7 px-6 border-0 rounded-lg font-size-h6" type="email" placeholder="ایمیل خود را وارد فرمایید" name="email" autocomplete="off" />
                             </div>
                             <!--end::Form group-->
                             <!--begin::Form group-->
@@ -35,23 +49,13 @@
                             </div>
                             <!--end::Form group-->
                         </form>
+                        <!--end::Form-->
                     </div>
-                    <!--end::Signin-->
+                    <!--end::Forgot-->
                 </div>
                 <!--end::Wrapper-->
             </div>
-            <!--begin::Content-->
-            <!--begin::Aside-->
-            <div class="login-aside order-1 order-lg-2 bgi-no-repeat bgi-position-x-right">
-                <div class="login-conteiner bgi-no-repeat bgi-position-x-right bgi-position-y-bottom" style="background-image: url(assets/media/svg/illustrations/login-visual-4.svg);">
-                    <!--begin::Aside title-->
-                    {{--<h3 class="pt-lg-40 pl-lg-20 pb-lg-0 pl-10 py-20 m-0 d-flex justify-content-lg-start font-weight-boldest display5 display1-lg text-white">We Got--}}
-                        {{--<br />A Surprise--}}
-                        {{--<br />For You</h3>--}}
-                    <!--end::Aside title-->
-                </div>
-            </div>
-            <!--end::Aside-->
+            <!--end::Content-->
         </div>
         <!--end::Login-->
     </div>
