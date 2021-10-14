2ac5c667a7193135341481522e51cb9de20f0890
raya-team <rayateam.net@gmail.com>
2021-10-14 10:40:00 +0330

    mahdy

--- app/Http/Controllers/user/JustificationPlan/JustificationPlanController.php
+++ app/Http/Controllers/user/JustificationPlan/JustificationPlanController.php
@@ -15,10 +15,11 @@ class JustificationPlanController extends Controller
 {
     public function index()
     {
-        $registered_team = RegisteredTeam::where('team_id' , Auth::user()->team_id)->first();
-        $business_manager = BusinessManager::where('team_id' , Auth::user()->team_id)->first();
-        $business_question = BusinessQuestion::where('team_id' , Auth::user()->team_id)->first();
-        $justification_plan = PreliminaryJustificationPlan::where('team_id' , Auth::user()->team_id)->first();
+        $team_id = Auth::user()->team_id;
+        $registered_team = RegisteredTeam::where('team_id' , $team_id)->first();
+        $business_manager = BusinessManager::where('team_id' , $team_id)->first();
+        $business_question = BusinessQuestion::where('team_id' , $team_id)->first();
+        $justification_plan = PreliminaryJustificationPlan::where('team_id' , $team_id)->first();
         return view('user.justification-plan.index');
     }
 
