f60389bf2eec7f2ff5af5b76738243735b0c2173
Ehsan <ehsanroozbakhsh@gmail.com>
2021-12-12 10:21:40 +0330

    Ehsan

--- routes/web/web.php
+++ routes/web/web.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect(route('login'));
-    \Illuminate\Support\Facades\Auth::logout();
-    return view('welcome');
 });
 Route::get('/auth-check', function () {
     $user = \Illuminate\Support\Facades\Auth::check();
